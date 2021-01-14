e9259698cd01e5088b00e5eb95d675bbd0964ef9
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2021-01-14 19:03:55 +0200

    basic backend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+let names =  [
+          {
+            "name": "Ville",
+            "amount": 24
+          },
+          {
+            "name": "Anna",
+            "amount": 6
+          },
+          {
+            "name": "Antti",
+            "amount": 22
+          },
+          {
+            "name": "Sanna",
+            "amount": 5
+          },
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/names', (req, res) => {
+    res.json(names)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
